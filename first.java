3944279e06ab486525f76e465a8da97ffcc42023
Dev <dev434098@gmail.com>
2025-07-20 10:39:57 +0530

    deleted

--- first.java
+++
@@ -1,7 +0,0 @@
-public class first {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int a=3;
-        System.out.println("Value of a is: " + a);
-    }
-}
